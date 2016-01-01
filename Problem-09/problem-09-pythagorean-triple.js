e49195626b7bd1dca0f292631f7cc03d6b898046
David Danaher <ddanaher@ch-nb-469.local>
2016-01-01 17:35:34 -0600

    Problem-09: complete

--- Problem-09/problem-09-pythagorean-triple.js
+++ Problem-09/problem-09-pythagorean-triple.js
@@ -7,16 +7,12 @@ For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
 There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc.
 
-Generate sets of a b and c that add to 1000 that are perfect squares
-
 */
 
 var startTime = new Date();;
 var endTime;
 
 console.log('Problem 8: The solution [' + productOfTriple(1000) + '] was found in [' + getDuration(endTime, startTime) + '] s ');
-
-// console.log(productOfTriple(100));
 
 function productOfTriple(value) {
     var limit = Math.pow(value, 2);
@@ -72,12 +68,6 @@ function generateSquaresBelow(value) {
 function isPerfectSquare(value) {
     return Math.sqrt(value) % 1 === 0;
 }
-
-
-
-
-
-
 
 function getDuration(endTime, startTime) {
     var end = endTime.getSeconds() * 1000 + endTime.getMilliseconds();
